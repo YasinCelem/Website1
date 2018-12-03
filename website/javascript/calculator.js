5c1299bbdb44caf27dd2e65688ee89033ecf6e4a
YasinCelem <44833238+YasinCelem@users.noreply.github.com>
2018-12-03 16:28:39 +0100

    Add files via upload

---
+++ website/javascript/calculator.js
@@ -0,0 +1,21 @@
+function btnplus()
+{	document.Calculator.display.value +="+";
+document.Calculator.display.textAlign="right";}
+function btnsub()
+{	document.Calculator.display.value +="-";
+document.Calculator.display.textAlign="right";}
+function btnmult()
+{	document.Calculator.display.value +="*";
+document.Calculator.display.textAlign="right";}
+function btnMod()
+{	document.Calculator.display.value +="%";
+document.Calculator.display.textAlign="right";}
+function btndot()
+{	document.Calculator.display.value +=",";
+document.Calculator.display.textAlign="right";}
+function btndiv()
+{	document.Calculator.display.value +="/";
+document.Calculator.display.textAlign="right";}
+function btnclear()
+{	document.Calculator.display.value ="";
+document.Calculator.display.textAlign="right";}
